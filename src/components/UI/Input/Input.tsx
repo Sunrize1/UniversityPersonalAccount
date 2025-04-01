0426ee9d8d6bf509c61ae8bf208bab626b080ff0
Sunrize1 <vladislaw.2000.pro@gmail.com>
2025-04-01 18:55:45 +0700

    fix input

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -2,11 +2,12 @@ import { inputProps } from "../../../types/components/UI/input"
 import styles from './Input.module.css';
 import typography from '../../../styles/typography.module.css';
 
-export const Input = ({label, error, ...rest}: inputProps) => {
+export const Input = ({label, disabled, error, ...rest}: inputProps) => {
   const textFieldClasses = [
     styles.textField,
     typography.p2,
     error ? styles.error : '',
+    disabled ? styles.disabled : '',
   ].filter(Boolean).join(' ');
 
     return (
@@ -14,7 +15,7 @@ export const Input = ({label, error, ...rest}: inputProps) => {
       <div className={textFieldClasses}>
         <label className={styles.labelText}>{label}</label>
         <div className={styles.content}>
-          <input className={styles.inputText} type="text" placeholder="Введите текст" />
+          <input className={styles.inputText} disabled={disabled} {...rest} />
         </div>
       </div>
       <div>
